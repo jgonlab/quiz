3d94ef9fb075aed129ab63eb82c28f2c4d92f8d0
Jorge <jgonlab@gmail.com>
2015-07-29 18:57:46 +0200

    Con campo tema en BBDD

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -44,7 +44,7 @@ exports.author = function(req,res){
 //GET /quizes/new
 exports.new = function(req,res){
     var quiz = models.Quiz.build(   //Crea objeto quiz
-        {pregunta: "Pregunta", respuesta: "Respuesta"}
+        {pregunta: "Pregunta", respuesta: "Respuesta", tema: "otro"}
     );
     res.render('quizes/new', {quiz: quiz, errors: []});
 }
@@ -63,7 +63,7 @@ exports.create = function(req, res) {
             }
             else{
                 // guarda en DB los campos pregunta y respuesta de quiz
-                quiz.save({fields: ["pregunta", "respuesta"]}).then(function(){
+                quiz.save({fields: ["pregunta", "respuesta", "tema"]}).then(function(){
                     res.redirect('/quizes')})
             }  //Redirección HTTP (URL relativo) lista de preguntas
         }
@@ -94,6 +94,7 @@ exports.edit = function(req,res){
 exports.update = function(req,res){
     req.quiz.pregunta = req.body.quiz.pregunta;
     req.quiz.respuesta = req.body.quiz.respuesta;
+    req.quiz.tema = req.body.quiz.tema;
     
     req.quiz.validate().then(
         function(err){
@@ -101,7 +102,7 @@ exports.update = function(req,res){
                 res.render('quizes/edit', {quiz: req.quiz, errors: err.errors});
             } else {
                 req.quiz
-                .save({fields: ["pregunta","respuesta"]})
+                .save({fields: ["pregunta","respuesta","tema"]})
                 .then( function(){res.redirect('/quizes');});
             }
         }
--- models/models.js
+++ models/models.js
@@ -37,10 +37,12 @@ sequelize.sync().then(function() {
     Quiz.count().success(function (count){
         if (count === 0){
             Quiz.create({ pregunta: 'Capital de Italia',
-                         respuesta: 'Roma'
+                         respuesta: 'Roma',
+                         tema: 'geografia'
                        });
             Quiz.create({ pregunta: 'Capital de Portugal',
-                         respuesta: 'Lisboa'
+                         respuesta: 'Lisboa',
+                         tema: 'geografia'
                        })
             .then(function(){console.log('Base de datos inicializada')});
         };
