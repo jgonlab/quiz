51c09d842f85b8b3d2fb2d33d89ddabab60e0c4d
Jorge <jgonlab@gmail.com>
2015-08-10 19:46:56 +0200

    Moderación de comentarios

--- controllers/comment_controller.js
+++ controllers/comment_controller.js
@@ -1,6 +1,22 @@
 var models = require('../models/models.js');
 
-//GET /quizes/new
+//Autoload :id de comentarios
+exports.load = function(req, res, next, commentId){
+    models.Comment.find({
+        where: {
+            id: Number(commentId)
+        }
+    }).then(function(comment){
+        if (comment){
+            req.comment = comment;
+            next();
+        } else{ next(new Error('No existe comentId=' + commentId))}
+    }
+    ).catch(function(error){next(error)});
+};
+
+
+//GET /quizes/:quizId/comments/new
 exports.new = function(req,res){
     res.render('comments/new', {quizId: req.params.quizId, errors: []});
 }
@@ -28,4 +44,12 @@ exports.create = function(req, res) {
             }  //Redirección HTTP (URL relativo) lista de preguntas
         }
     ).catch(function(error){next(error)});
+};
+
+// GET /quizes/:quizId/comments/:commentId/publish
+exports.publish = function(req, res){
+    req.comment.publicado = true;
+    req.comment.save( {fields: ["publicado"]})
+    .then( function(){ res.redirect('/quizes/'+req.params.quizId);})
+    .catch(function(error){next(error)});
 };
--- routes/index.js
+++ routes/index.js
@@ -12,6 +12,7 @@ router.get('/', function(req, res) {
 
 //Autoload de comandos con :quizId
 router.param('quizId',quizcontroller.load); //autoload :quizId
+router.param('commentId',commentController.load); //autoload :commentId
 
 //Definición de rutas de sesión
 router.get('/login', sessionController.new);
@@ -29,7 +30,9 @@ router.get('/quizes/:quizId(\\d+)/edit', sessionController.loginRequired, quizco
 router.put('/quizes/:quizId(\\d+)', sessionController.loginRequired, quizcontroller.update);
 router.delete('/quizes/:quizId(\\d+)', sessionController.loginRequired, quizcontroller.destroy);
 
+//Definición de rutas de comentarios
 router.get('/quizes/:quizId(\\d+)/comments/new', commentController.new);
 router.post('/quizes/:quizId(\\d+)/comments', commentController.create);
+router.get('/quizes/:quizId(\\d+)/comments/:commentId(\\d+)/publish', sessionController.loginRequired, commentController.publish);
 
 module.exports = router;
